1d376ec10bd0f2ac54a7db2e1e566f862dd3a7e2
paulclindo <paulclindo@gmail.com>
2023-06-08 13:35:00 -0500

    fix: error handling

--- src/handlers/invoice.ts
+++ src/handlers/invoice.ts
@@ -1,86 +1,70 @@
 import prisma from "../db";
 
 // Get all
-export const getInvoices = async (req, res, next) => {
-  try {
-    const invoice = await prisma.invoice.findMany({
-      where: {},
-      include: {
-        invoiceItems: true,
-      },
-    });
+export const getInvoices = async (req, res) => {
+  const invoice = await prisma.invoice.findMany({
+    where: {},
+    include: {
+      invoiceItems: true,
+    },
+  });
 
-    res.json({ data: invoice });
-  } catch (error) {
-    next(error);
-  }
+  res.json({ data: invoice });
 };
 
 // Get one
-export const getOneInvoice = async (req, res, next) => {
-  try {
-    const id = req.params.id;
+export const getOneInvoice = async (req, res) => {
+  const id = req.params.id;
 
-    const invoice = await prisma.invoice.findFirst({
-      where: {
-        id,
-      },
-      include: {
-        invoiceItems: true,
-      },
-    });
+  const invoice = await prisma.invoice.findFirst({
+    where: {
+      id,
+    },
+    include: {
+      invoiceItems: true,
+    },
+  });
 
-    res.json({ data: invoice });
-  } catch (error) {
-    next(error);
-  }
+  res.json({ data: invoice });
 };
 
 // Create one
-export const createInvoice = async (req, res, next) => {
-  try {
-    const invoice = await prisma.invoice.create({
-      data: {
-        ...req.body,
-        invoiceItems: {
-          create: req.body.invoiceItems,
-        },
+export const createInvoice = async (req, res) => {
+  const invoice = await prisma.invoice.create({
+    data: {
+      ...req.body,
+      invoiceItems: {
+        create: req.body.invoiceItems,
       },
-    });
+    },
+  });
 
-    res.json({ data: invoice });
-  } catch (error) {
-    next(error);
-  }
+  res.json({ data: invoice });
 };
 
 // Update one
-export const updateInvoice = async (req, res, next) => {
-  try {
-    const invoice = await prisma.invoice.update({
-      where: {
-        id: req.body.id,
+export const updateInvoice = async (req, res) => {
+  const invoice = await prisma.invoice.update({
+    where: {
+      id: req.body.id,
+    },
+    data: {
+      ...req.body,
+      invoiceItems: {
+        upsert: req.body.invoiceItems
+          ? req.body.invoiceItems.map((item) => ({
+              where: {
+                id: item.id,
+              },
+              update: item,
+              create: item,
+            }))
+          : [],
       },
-      data: {
-        ...req.body,
-        invoiceItems: {
-          upsert: req.body.invoiceItems
-            ? req.body.invoiceItems.map((item) => ({
-                where: {
-                  id: item.id,
-                },
-                update: item,
-                create: item,
-              }))
-            : [],
-        },
-      },
-    });
+    },
+  });
 
-    res.json({ data: invoice });
-  } catch (error) {
-    next(error);
-  }
+  res.json({ data: invoice });
 };
 
 // Delete one
@@ -95,34 +79,26 @@ export const updateInvoice = async (req, res, next) => {
 // };
 //
 // delete an invoice
-export const deleteInvoice = async (req, res, next) => {
-  try {
-    const invoiceId = req.params.id;
+export const deleteInvoice = async (req, res) => {
+  const invoiceId = req.params.id;
 
-    const deleted = await prisma.invoice.delete({
-      where: {
-        id: invoiceId,
-      },
-    });
+  const deleted = await prisma.invoice.delete({
+    where: {
+      id: invoiceId,
+    },
+  });
 
-    res.json({ data: deleted });
-  } catch (error) {
-    next(error);
-  }
+  res.json({ data: deleted });
 };
 
-export const deleteInvoiceItem = async (req, res, next) => {
-  try {
-    const invoiceItemId = req.params.id;
+export const deleteInvoiceItem = async (req, res) => {
+  const invoiceItemId = req.params.id;
 
-    const deleted = await prisma.invoiceItem.delete({
-      where: {
-        id: invoiceItemId,
-      },
-    });
+  const deleted = await prisma.invoiceItem.delete({
+    where: {
+      id: invoiceItemId,
+    },
+  });
 
-    res.json({ data: deleted });
-  } catch (error) {
-    next(error);
-  }
+  res.json({ data: deleted });
 };
